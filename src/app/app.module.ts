4d84f661ecd8b075fff6368dcf515ce176734614
pathjo0826 <pathjo0826@skola.goteborg.se>
2024-11-28 11:16:47 +0100

    Added functionality: Routing to Views from the left side menu bar. Reactive behaviour of the left side menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { ScheduleComponent } from './views/schedule/schedule.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './views/error/error.component';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
+  {path: 'Home', component: HomeComponent},
+  {path: 'About', component: AboutComponent},
+  {path: 'Schedule', component: ScheduleComponent},
+  {path: 'Contact', component: ContactComponent},
+  {path: '**', component: ErrorComponent} 
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ScheduleComponent,
+    ContactComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
