9ac1f36c6b533e022f506e6c104c41adf8c907fd
pathjo0826 <pathjo0826@skola.goteborg.se>
2024-12-07 19:32:43 +0100

    Wip. Adding text to image in image collage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './views/error/error.component';
 import { TimerComponent } from './components/timer/timer.component';
 import { RsvpComponent } from './views/rsvp/rsvp.component';
-//import { RegistryComponent } from './views/registry/registry.component';
+import { RegistryComponent } from './views/registry/registry.component';
 
 
 const appRoutes: Routes = [
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
   {path: 'Home', component: HomeComponent},
   {path: 'About', component: AboutComponent},
   {path: 'Schedule', component: ScheduleComponent},
-  //{path: 'Registry', component: RegistryComponent}, 
+  {path: 'Registry', component: RegistryComponent}, 
   {path: 'Contact', component: ContactComponent},
   {path: 'Rsvp', component: RsvpComponent},
   {path: '**', component: ErrorComponent} 
@@ -40,7 +40,7 @@ const appRoutes: Routes = [
     ErrorComponent,
     TimerComponent,
     RsvpComponent,
-    //RegistryComponent
+    RegistryComponent
   ],
   imports: [
     BrowserModule,
