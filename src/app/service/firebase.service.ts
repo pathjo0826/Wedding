2eaa0af07847140bc09a0503fa2df9fea930a5cc
pathjo0826 <pathjo0826@skola.goteborg.se>
2024-12-10 13:38:17 +0100

    Wip, fetchGifts and updateClaim has been moved to a service class

--- src/app/service/firebase.service.ts
+++ src/app/service/firebase.service.ts
@@ -1,14 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { Gift } from '../domain/gift';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  private databaseUrl = 'https://wedding-29b48-default-rtdb.europe-west1.firebasedatabase.app/';
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient, private firestore: AngularFirestore) { }
+  fetchGifts(): Observable<any>  {
+
+    const url = 'https://wedding-29b48-default-rtdb.europe-west1.firebasedatabase.app/gifts.json';
+    return this.http.get<Gift[]>(url).pipe(map((resp) => {
+      const gifts = [];
+      for (const gift in resp) {
+        gifts.push({ ...resp[gift] });
+      }
+      return gifts;
+    }));
+  }
+
+  updateClaim(path: string, data: any): Observable<any> {
+    
+    const databaseUrl = "https://wedding-29b48-default-rtdb.europe-west1.firebasedatabase.app/";
+    const url = databaseUrl + "/" + path + ".json";
+
+    return this.http.patch(url, data);
+  }
 }
--- src/app/views/registry/registry.component.ts
+++ src/app/views/registry/registry.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Gift } from 'src/app/domain/gift';
+import { FirebaseService } from 'src/app/service/firebase.service';
 
 @Component({
   selector: 'app-registry',
@@ -12,13 +13,19 @@ export class RegistryComponent implements OnInit {
 
   giftList: Gift[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private firebaseService: FirebaseService) {}
 
   ngOnInit(): void {
     this.fetchGifts()
   }
 
   fetchGifts() {
+    this.firebaseService.fetchGifts().subscribe((gifts) => {
+      this.giftList = gifts;
+    })
+  }
+
+  fetchGifts_old() {
 
     try {
       const url = 'https://wedding-29b48-default-rtdb.europe-west1.firebasedatabase.app/gifts.json';
@@ -40,7 +47,7 @@ export class RegistryComponent implements OnInit {
     const path = "gifts/gift_2";
     const data = { claimed: false };
 
-    this.myUpdate(path, data).subscribe(() => {
+    this.firebaseService.updateClaim(path, data).subscribe(() => {
       try {
         console.log("Update successful");
         this.ngOnInit();
@@ -49,11 +56,5 @@ export class RegistryComponent implements OnInit {
         console.error("Update failed", error);
       }
     })
-  }
-
-  myUpdate(path: string, data: any): Observable<any> {
-    const databaseUrl = "https://wedding-29b48-default-rtdb.europe-west1.firebasedatabase.app/";
-    const url = `${databaseUrl}/${path}.json`;
-    return this.http.patch(url, data);
   }
 }
